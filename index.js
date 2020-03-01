208aea38a5875f6b5ff84ed6a21bac28ada3b5f2
PTChinh <PTChinh@users.noreply.github.com>
2020-03-01 14:47:43 +0700

    update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening on 
 app.post('/webhook', (req, res) => {
 
     let body = req.body;
-    console.log(body);
+    console.log(body.entry.messaging);
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
 
